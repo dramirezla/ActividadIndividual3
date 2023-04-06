625780a0aab2fdc3fafe863da1745b391ce77c7f
dramirezla <53583408+dramirezla@users.noreply.github.com>
2023-04-05 22:02:11 -0500

    Add files via upload

---
+++ FigurasGeom/src/main/java/com/mycompany/figurasgeom/FigurasGeom.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figurasgeom;
+
+/**
+ *
+ * @author David R.L
+ */
+public class FigurasGeom {
+
+    public static void main(String[] args) {
+        InterfazUsuario formulario=new InterfazUsuario();
+        formulario.setVisible(true);
+
+    }
+}
